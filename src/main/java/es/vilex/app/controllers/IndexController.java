06fc9fe66bbaa8cb0102dfa41c985aff6d7e67f2
Hector Mendez <hjmendez@atsistemas.com>
2018-11-26 12:45:31 +0100

    version inicial

---
+++ src/main/java/es/vilex/app/controllers/IndexController.java
@@ -0,0 +1,30 @@
+/**
+ * Aviso legal
+ * 
+ * Toda la información contenida aquí es propiedad de Diners Club Spain, S.A. y está protegida por
+ * la ley de propiedad intelectual. Cualquier difusión o reproducción total o parcial, incluso para
+ * uso por personal interno (empleado) o externo (proveedor), por cualquier medio y bajo cualquier
+ * forma, está prohibida, salvo autorización expresa por parte de Diners Club Spain, otorgada con
+ * carácter previo y de forma escrita.
+ * 
+ * El uso o acceso permitido a dicha información no podrá entenderse como cesión de ninguna clase de
+ * derecho de explotación sobre los citados derechos de propiedad intelectual.
+ */
+
+
+package es.vilex.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("msg", "hola mundo");
+    return "index";
+  }
+
+}
